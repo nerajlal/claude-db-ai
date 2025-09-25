e4e99de945d9b46809fcb4023f9cafd60a690193
neraj <nerajnerajlal@gmail.com>
2025-09-25 10:20:02 +0530

    test

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::middleware(['auth'])->group(function () {
 });
 
 
+
+Route::get('/test', function () {
+    return view('test'); // this will load resources/views/test.blade.php
+})->name('test');
